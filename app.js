fc7ca31daed672a3bc3e207c5ad4a9a6c2ac27ca
unknown <muhammadzeeshanali8@gmail.com>
2020-06-05 23:20:04 +0500

    E-Commerce Demo

---
+++ app.js
@@ -0,0 +1,105 @@
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+//   for view product
+/* Open when someone clicks on the span element */
+function openNav1() {
+    document.getElementById("myNav1").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav1() {
+    document.getElementById("myNav1").style.width = "0%";
+  }
+   /* Close button down*/
+  function closeNav1() {
+    document.getElementById("myNav1").style.width = "0%";
+  }
+
+function openNav2() {
+    document.getElementById("myNav2").style.width = "100%";
+  }
+  
+ 
+  function closeNav2() {
+    document.getElementById("myNav2").style.width = "0%";
+  }
+  function closeNav2() {
+    document.getElementById("myNav2").style.width = "0%";
+  }
+  function openNav3() {
+    document.getElementById("myNav3").style.width = "100%";
+  }
+  
+ 
+  function closeNav3() {
+    document.getElementById("myNav3").style.width = "0%";
+  }
+  function closeNav3() {
+    document.getElementById("myNav3").style.width = "0%";
+  }
+  function openNav4() {
+    document.getElementById("myNav4").style.width = "100%";
+  }
+  
+ 
+  function closeNav4() {
+    document.getElementById("myNav4").style.width = "0%";
+  }
+  function closeNav4() {
+    document.getElementById("myNav4").style.width = "0%";
+  }
+  function openNav5() {
+    document.getElementById("myNav5").style.width = "100%";
+  }
+  
+ 
+  function closeNav5() {
+    document.getElementById("myNav5").style.width = "0%";
+  }
+  function closeNav5() {
+    document.getElementById("myNav5").style.width = "0%";
+  }
+  function openNav6() {
+    document.getElementById("myNav6").style.width = "100%";
+  }
+  
+ 
+  function closeNav6() {
+    document.getElementById("myNav6").style.width = "0%";
+  }
+  function closeNav6() {
+    document.getElementById("myNav6").style.width = "0%";
+  }
+  function openNav7() {
+    document.getElementById("myNav7").style.width = "100%";
+  }
+  
+ 
+  function closeNav7() {
+    document.getElementById("myNav7").style.width = "0%";
+  }
+  function closeNav7() {
+    document.getElementById("myNav7").style.width = "0%";
+  }
+
+  function openNav8() {
+    document.getElementById("myNav8").style.width = "100%";
+  }
+  
+ 
+  function closeNav8() {
+    document.getElementById("myNav8").style.width = "0%";
+  }
+  function closeNav8() {
+    document.getElementById("myNav8").style.width = "0%";
+  }
