3d589423fd502f756d826292dfb07096ccb2047f
muzeeali <64666168+muzeeali@users.noreply.github.com>
2021-04-06 03:15:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,105 +1,123 @@
 
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+
+ 
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+// database
+function db(){
+  var pt=document.getElementById("pt");
+  var pq=document.getElementById("pq");
+  var ea=document.getElementById("Ea");
+  var pn=document.getElementById("pn");
+  var pa=document.getElementById("pa");
+var cData={
+productTittle:pt.value,
+productQuantity:pq.value,
+emailAddress:ea.value,
+phoneNumber:pn.value,
+postalAddress:pa.value
+}
+firebase.database().ref('client details').push(cData)
+}
 
 
 //   for view product
 /* Open when someone clicks on the span element */
 function openNav1() {
-    document.getElementById("myNav1").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav1() {
-    document.getElementById("myNav1").style.width = "0%";
-  }
-   /* Close button down*/
-  function closeNav1() {
-    document.getElementById("myNav1").style.width = "0%";
-  }
+  document.getElementById("myNav1").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav1() {
+  document.getElementById("myNav1").style.width = "0%";
+}
+ /* Close button down*/
+function closeNav1() {
+  document.getElementById("myNav1").style.width = "0%";
+}
 
 function openNav2() {
-    document.getElementById("myNav2").style.width = "100%";
-  }
-  
- 
-  function closeNav2() {
-    document.getElementById("myNav2").style.width = "0%";
-  }
-  function closeNav2() {
-    document.getElementById("myNav2").style.width = "0%";
-  }
-  function openNav3() {
-    document.getElementById("myNav3").style.width = "100%";
-  }
-  
- 
-  function closeNav3() {
-    document.getElementById("myNav3").style.width = "0%";
-  }
-  function closeNav3() {
-    document.getElementById("myNav3").style.width = "0%";
-  }
-  function openNav4() {
-    document.getElementById("myNav4").style.width = "100%";
-  }
-  
- 
-  function closeNav4() {
-    document.getElementById("myNav4").style.width = "0%";
-  }
-  function closeNav4() {
-    document.getElementById("myNav4").style.width = "0%";
-  }
-  function openNav5() {
-    document.getElementById("myNav5").style.width = "100%";
-  }
-  
- 
-  function closeNav5() {
-    document.getElementById("myNav5").style.width = "0%";
-  }
-  function closeNav5() {
-    document.getElementById("myNav5").style.width = "0%";
-  }
-  function openNav6() {
-    document.getElementById("myNav6").style.width = "100%";
-  }
-  
- 
-  function closeNav6() {
-    document.getElementById("myNav6").style.width = "0%";
-  }
-  function closeNav6() {
-    document.getElementById("myNav6").style.width = "0%";
-  }
-  function openNav7() {
-    document.getElementById("myNav7").style.width = "100%";
-  }
-  
- 
-  function closeNav7() {
-    document.getElementById("myNav7").style.width = "0%";
-  }
-  function closeNav7() {
-    document.getElementById("myNav7").style.width = "0%";
-  }
+  document.getElementById("myNav2").style.width = "100%";
+}
 
-  function openNav8() {
-    document.getElementById("myNav8").style.width = "100%";
-  }
-  
- 
-  function closeNav8() {
-    document.getElementById("myNav8").style.width = "0%";
-  }
-  function closeNav8() {
-    document.getElementById("myNav8").style.width = "0%";
-  }
+
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
+function openNav3() {
+  document.getElementById("myNav3").style.width = "100%";
+}
+
+
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0%";
+}
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0%";
+}
+function openNav4() {
+  document.getElementById("myNav4").style.width = "100%";
+}
+
+
+function closeNav4() {
+  document.getElementById("myNav4").style.width = "0%";
+}
+function closeNav4() {
+  document.getElementById("myNav4").style.width = "0%";
+}
+function openNav5() {
+  document.getElementById("myNav5").style.width = "100%";
+}
+
+
+function closeNav5() {
+  document.getElementById("myNav5").style.width = "0%";
+}
+function closeNav5() {
+  document.getElementById("myNav5").style.width = "0%";
+}
+function openNav6() {
+  document.getElementById("myNav6").style.width = "100%";
+}
+
+
+function closeNav6() {
+  document.getElementById("myNav6").style.width = "0%";
+}
+function closeNav6() {
+  document.getElementById("myNav6").style.width = "0%";
+}
+function openNav7() {
+  document.getElementById("myNav7").style.width = "100%";
+}
+
+
+function closeNav7() {
+  document.getElementById("myNav7").style.width = "0%";
+}
+function closeNav7() {
+  document.getElementById("myNav7").style.width = "0%";
+}
+
+function openNav8() {
+  document.getElementById("myNav8").style.width = "100%";
+}
+
+
+function closeNav8() {
+  document.getElementById("myNav8").style.width = "0%";
+}
+function closeNav8() {
+  document.getElementById("myNav8").style.width = "0%";
+}
